bc4d3573a2142f327f9f89f053f9d3700d47917d
Rodrigo Ramos <rodrigo.ramos@intelligential.tech>
2022-08-31 13:57:08 -0500

    [UPDATE] cors added

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import * as cors from 'cors';
 import { routerApi } from '../Routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const allowlist = [
+  'https://dochain.vercel.app/',
+  `http://localhost:${PORT}/`
+]
+
+const options = {
+  origin: (origin, cb) => {
+    if (allowlist.includes(origin) || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error('not allowed'))
+    }
+  }
+}
+
 app.use(express.json());
+app.use(cors(options));
 
 app.get('/', (req, res, next) => {
   res.redirect('/api/v1/documents')
@@ -13,5 +30,5 @@ app.get('/', (req, res, next) => {
 routerApi(app);
 
 app.listen(PORT, () => {
-  console.log("Listening in http://localhost:" + PORT + "/");
+  console.log(`Listening in http://localhost:${PORT}/`);
 });
