a53ebc22b09fecd0dcb9141e05401a21ff194d5e
Rodrigo Ramos Xochiteotzin <rodrx20@gmail.com>
2022-09-07 13:57:22 -0500

    [ADD] briefcase model | sequelize

---
+++ Db/models/briefcaseModel.js
@@ -0,0 +1,67 @@
+import { Model, DataTypes, Sequelize } from ('sequelize');
+
+const BRIEFCASE_TABLE = 'briefcases';
+
+const briefcaseSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  type: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  place: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  crime: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  dateCrime: {
+    allowNull: false,
+    type: DataTypes.DATE,
+  },
+  placeCrime: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  nameDefendant: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  nameComplainant: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  lawyerDefendant: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  lawyerComplainant: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+}
+
+class briefcase extends Model {
+  static associate() {
+    // associate
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: BRIEFCASE_TABLE,
+      modelName: 'briefcase',
+      timestamps: false
+    }
+  }
+}
+
+
+export { BRIEFCASE_TABLE, briefcaseSchema, briefcase };
